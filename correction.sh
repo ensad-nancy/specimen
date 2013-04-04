1a7a8bcc6ecc67dfa14168ae8795b81111cd8aa2
dcfvg <qwsx@dcfvg.com>
2013-04-04 13:19:10 +0200

    clean sh adjustement script
    Former-commit-id: ab40956548500b9c06ee7dc10eb7d4a892b1fee0

--- correction.sh
+++ correction.sh
@@ -1,9 +1,6 @@
 c=25
-# for f in `find ~/Desktop/png512-disque/ -name "*.jpg"`
 for f in `find content/01-fonts/ -name "*.JPG"`
 do
-   # innercrop -f 4 -m crop $f .$f
-   # unperspective -f 20 $f .$f
    echo $f
    convert $f -fuzz 50% -crop +$c+$c -crop -$c-$c -border $cx$c -transparent white $f.png
    rm $f
