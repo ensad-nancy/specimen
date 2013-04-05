cddee713ba41bfb0a0d83b77138e05b11292a47d
dcfvg <qwsx@dcfvg.com>
2013-04-05 07:56:46 +0200

    Cleaning code, recrop, log
    Former-commit-id: 9c00dbffef2e3680020c7334407e75331e13327a

--- correction.sh
+++ correction.sh
@@ -1,7 +1,13 @@
 c=25
+echo 'refreshing specimen …'
+
 for f in `find content/01-fonts/ -name "*.JPG"`
 do
    echo $f
-   convert $f -fuzz 50% -crop +$c+$c -crop -$c-$c -border $cx$c -transparent white $f.png
+   convert $f -fuzz 50% -crop +$c+$c -crop -$c-$c -border $cx$c -crop -0-100 -crop +0+100 -transparent white $f.png
+   
+   # -crop -0-100 -crop +0+100
    rm $f
 done
+ 
+echo 'done'
